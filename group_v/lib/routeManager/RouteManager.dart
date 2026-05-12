c58147cd3911bfa753f4a32598af4ab86eff4f06
LitebohoJL <223002326@stud.ac.za>
2026-05-11 19:10:50 -0800

    worked on students file and routeManager file (errors need to be fixed)

---
+++ group_v/lib/routeManager/RouteManager.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:group_v/models/Students.dart';
+import 'package:group_v/views/Admin_Dashboard.dart';
+import 'package:group_v/views/Application_Detail_Screen.dart';
+import 'package:group_v/views/Home_Screen.dart';
+import 'package:group_v/views/StudentAssistantApplicationForm.dart';
+
+class RouteManager {
+  //Static routes
+  static const String home = '/';
+  static const String details = '/details';
+  static const String applicationForm = '/applicationForm';
+  static const String adminDashboard = '/adminDashboard';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home:
+        return MaterialPageRoute(builder: (_) => const HomeView());
+
+      case details:
+        final message = settings.arguments as String; // dynamic route
+        return MaterialPageRoute(
+          builder: (_) => StudentDetailsPage(message: message),
+        );
+
+      case applicationForm:
+        return MaterialPageRoute(builder: (_) => const EditStudentPage());
+
+      case adminDashboard:
+        return MaterialPageRoute(builder: (_) => const ConfirmView());
+
+      default:
+        throw Exception("Route not found");
+    }
+  }
+}
